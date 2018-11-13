f9854e6e8bc468878bcdfe80223d162976c16eb4
Unknown <shiozaki5621@gmail.com>
2018-11-13 03:07:24 -0800

    Research for let and var

---
+++ LetVar.js
@@ -0,0 +1,21 @@
+// review for let
+let x = 1
+console.log(x)
+
+{
+    let x = 2
+    console.log(x)
+}
+
+console.log(x)
+
+//review for var
+var y = 'a'
+console.log(y)
+
+{
+    var y = 'b'
+    console.log(y);    
+}
+
+console.log(y);
