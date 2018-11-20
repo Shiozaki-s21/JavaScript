6c3c1a38ab3425cc842997c43eff680d3c764f1b
Unknown <shiozaki5621@gmail.com>
2018-11-20 14:53:45 -0800

    Add code for study about random

--- Random/MathRandom.js
+++ Random/MathRandom.js
@@ -1,1 +1,8 @@
-console.log(Math.random() * 10);
+// expect to 0 to 100
+console.log(Math.round(Math.random() * 100));
+
+// expect to 1 to 100
+console.log(Math.ceil(Math.random() * 100));
+
+// expect to 0 to 99
+console.log(Math.floor(Math.random() * 100));
